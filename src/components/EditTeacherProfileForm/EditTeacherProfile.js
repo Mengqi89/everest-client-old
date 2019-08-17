4dc2c876bdd0502f39660d41ce7f681b8c6fa949
Kyler <Krenneker16@gmail.com>
2019-08-16 22:51:59 -0400

    initial commit for editTeacherProfileForm

---
+++ src/components/EditTeacherProfileForm/EditTeacherProfile.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class EditTeacherProfile extends Component {
+    render() {
+        return (
+            <h1>Edit Teacher Profile</h1>
+        )
+    }
+}
