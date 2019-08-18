2149c1bb04830842dc198c0a8c6869e7deecded3
Kyler <Krenneker16@gmail.com>
2019-08-17 23:24:29 -0400

    edit teacher profile without logic

--- src/components/EditTeacherProfileForm/EditTeacherProfile.js
+++ src/components/EditTeacherProfileForm/EditTeacherProfile.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react'
 import NationalityList from '../TeacherRegistrationForm/NationalityList'
-import './EditTeacherProfile.css'
+import './EditTeacherProfile.scss'
 import TeacherApiService from '../../services/teacher-api-service'
 import UserContext from '../../contexts/UserContext'
 
 class EditTeacherProfile extends Component {
     static contextType = UserContext
 
-    state = {
-       
-    }
-
     componentDidMount() {
         TeacherApiService.getTeacherProfile()
             .then(teacher => {
                 this.context.setUser(teacher)
-                this.setState(teacher)
             })
             .catch(err => console.log(err))
     }
@@ -26,6 +21,7 @@ class EditTeacherProfile extends Component {
         return (
             <div>
             <h1>Edit Teacher Profile</h1>
+                
                 <form className='form edit-teacher'>
                     {/* <div role='alert'>
                         {error && <p className='red'>{error}</p>}
@@ -37,7 +33,7 @@ class EditTeacherProfile extends Component {
                     <label htmlFor='teacher-password'>Enter a Password</label>
                     <input id='teacher-password' type='password' 
                     title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more 
-                    characters" name='password' placeholder='please enter your previous or new password' required></input>
+                    characters" name='password' placeholder='please enter password' required></input>
 
                     <label htmlFor='teacher-firstname'>First Name</label>
                     <input id='teacher-firstname' type='text' name='first_name' defaultValue={user.first_name} required></input>
@@ -56,10 +52,10 @@ class EditTeacherProfile extends Component {
                     </select>
 
                     <label htmlFor='teacher-nationality'>Nationality</label>
-                    <NationalityList id='teacher-nationality'/>
+                    <NationalityList id='teacher-nationality' nationality={user.nationality}/>
 
                     <label htmlFor='race'>Ethnicity</label>
-                    <select id='race' name='race' value={user.race} required>
+                    <select id='race' name='race' defaultValue={user.race} required>
                         <option value="" disabled>--select one--</option>
                         <option value='African American'>African American</option>
                         <option value='Asian'>Asian</option>
@@ -72,33 +68,33 @@ class EditTeacherProfile extends Component {
                     </select>
 
                     <label htmlFor='teacher-native-speaker'>Are You A Native Speaker?</label>
-                    <select id='teacher-native-speaker' name='native_speaker' value={user.native_speaker} required>
+                    <select id='teacher-native-speaker' name='native_speaker' defaultValue={user.native_speaker} required>
                         <option value='true'>Yes</option>
                         <option value='false'>No</option>
                     </select>
 
                     <label htmlFor='teacher-married'>Marraige Satus</label>
-                    <select id='teacher-married' name='married' value={user.married} required>
+                    <select id='teacher-married' name='married' defaultValue={user.married} required>
                         <option value="" disabled>--select one--</option>
                         <option value='Married'>married</option>
                         <option value='Single'>single</option>
                     </select>
 
                     <label htmlFor='teacher-education'>Education</label>
-                    <select id="teacher-education" name="highest_degree" defaultValue="" required>
+                    <select id="teacher-education" name="highest_degree" defaultValue={user.highest_degree} required>
                         <option value="" disabled="disabled">-- select one --</option>
                         <option value="No formal education">No formal education</option>
                         <option value="Primary education">Primary education</option>
                         <option value="Secondary education">Secondary education or high school</option>
                         <option value="GED">GED</option>
                         <option value="Vocational qualification">Vocational qualification</option>
-                        <option value="Bachelor's degree">Bachelor's degree</option>
+                        <option value="BA">Bachelor's degree</option>
                         <option value="Master's degree">Master's degree</option>
                         <option value="Doctorate or higher">Doctorate or higher</option>
                     </select>
 
                     <label htmlFor='teacher-degree'>Degree</label>
-                    <select id='teacher-degree' name='field_of_degree' defaultValue="No Degree" required>
+                    <select id='teacher-degree' name='field_of_degree' defaultValue={user.field_of_degree} required>
                         <option value='No Degree'>No Degree</option>
                         <option value='English'>English</option>
                         <option value='Business'>Business</option>
@@ -108,7 +104,7 @@ class EditTeacherProfile extends Component {
                     </select>
 
                     <label htmlFor='teacher-school'>School Attended</label>
-                    <input id='teacher-school' type='text' name='school' defaultValue="None" required></input>
+                    <input id='teacher-school' type='text' name='school' defaultValue={user.school}required></input>
 
                     <label htmlFor='teacher-certification'>Certification</label>
                     <select id='teacher-certification' name='certification' defaultValue="None" required>
@@ -119,7 +115,7 @@ class EditTeacherProfile extends Component {
                     </select>
 
                     <label htmlFor='teacher-experience'>Years Of Experience</label>
-                    <select id='teacher-experience' name='years_of_experience' defaultValue='0' required>
+                    <select id='teacher-experience' name='years_of_experience' defaultValue={user.years_of_experience} required>
                         <option value='0'>0</option>
                         <option value='1'>1</option>
                         <option value='2'>2</option>
@@ -127,7 +123,7 @@ class EditTeacherProfile extends Component {
                     </select>
 
                     <label htmlFor='teacher-experience-inChina'>Years Of Experience Teaching In China</label>
-                    <select id='teacher-experience-inChina' name='years_in_china' defaultValue='0' required>
+                    <select id='teacher-experience-inChina' name='years_in_china' defaultValue={user.years_in_china} required>
                         <option value='0'>0</option>
                         <option value='1'>1</option>
                         <option value='2'>2</option>
@@ -135,7 +131,7 @@ class EditTeacherProfile extends Component {
                     </select>
 
                     <label htmlFor='teacher-experience-notChina'>Years Of Experience Teaching - Other Countries</label>
-                    <select id='teacher-experience-notChina' name='years_teaching_abroad' defaultValue='0' required>
+                    <select id='teacher-experience-notChina' name='years_teaching_abroad' defaultValue={user.years_teaching_abroad} required>
                         <option value='0'>0</option>
                         <option value='1'>1</option>
                         <option value='2'>2</option>
