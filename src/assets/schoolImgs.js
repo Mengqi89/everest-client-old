db105d2a3da1ecfa1f79fced84d5a0b968f700a6
nataliesun <natsun121@gmail.com>
2019-08-24 21:44:31 -0700

    added jobsPage img to readme and random job img

---
+++ src/assets/schoolImgs.js
@@ -0,0 +1,7 @@
+const imgs = [
+  'https://images.unsplash.com/photo-1565734777784-6e89609ed871?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1949&q=80',
+  'https://images.unsplash.com/photo-1494081307357-78e3fb01df9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1531429745839-827a6a45e040?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+]
+
+module.exports = imgs
